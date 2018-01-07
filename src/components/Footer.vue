5ae62e8fe82723cba6f8e46e744e1151b4d535c8
jackdgerrard <16762701+jackdgerrard@users.noreply.github.com>
2018-01-07 13:45:43 +0000

    custom sass

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,23 +4,23 @@
         <div class="row">
             <div class="col l6 s12">
                 <h5>Tempus</h5>
-                <p class="grey-text text-lighten-4">Created by <a href="https://github.com/Snowbird4">Rokas</a> and <a href="jackdgerard.github.io">Jack</a> Using: </p>
+                <p>Created by: <br/> <a href="https://github.com/Snowbird4">Rokas</a><br/> and <a href="http://jackdgerrard.github.io">Jack</a>  </p>
             </div>
             <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
+                <h5 class="white-text">Using:</h5>
                 <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                  <li><a  href="https://firebase.google.com/">Firebase</a></li>
+                  <li><a  href="http://materializecss.com/">Materialize</a></li>
+                  <li><a  href="https://vuejs.org/">Vue.JS</a></li>
+                  <li><a  href="https://slack.com/">Slack</a></li>
+                  <li><a  href="https://github.com">GitHub</a></li>
                 </ul>
               </div>
             </div>
           </div>
           <div class="footer-copyright">
             <div class="container">
-            © 2014 TempusProject
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            © 2014 Tempus Project
             </div>
           </div>
 </footer>
