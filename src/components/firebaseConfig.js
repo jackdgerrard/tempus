97bfcef3a90a1f6bec4d0d354c69cf9c6159af4e
Jack <dev.jackdgerrard@gmail.com>
2018-01-25 15:37:02 +0000

    Update firebaseConfig.js

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,8 +1,4 @@
 export default {
-  apiKey: "AIzaSyDcILDzdq07zABF5IjubMpPVwKZ-SMEeEM",
-  authDomain: "tempusvuejs.firebaseapp.com",
-  databaseURL: "https://tempusvuejs.firebaseio.com",
-  projectId: "tempusvuejs",
-  storageBucket: "tempusvuejs.appspot.com",
-  messagingSenderId: "527943828063"
+  //firebase details when you create a database
+[YOUR-API-DETAILS-HERE]
 };
